ffb18c58d2abbc3d1de62165f0d58f7d889c28e0
s21974 <ksuchomski@hotmail.com>
2024-01-28 15:15:05 +0100

    Utworzenie pustego projektu

---
+++ src/main/java/com/example/s21974_bank/S21974BankApplication.java
@@ -0,0 +1,13 @@
+package com.example.s21974_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S21974BankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S21974BankApplication.class, args);
+    }
+
+}
