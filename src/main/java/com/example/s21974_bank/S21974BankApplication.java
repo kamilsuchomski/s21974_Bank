fbf338d7e9f4a19252521127fc3bbcb7e94c39e1
kamilsuchomski <ksuchomski@hotmail.com>
2024-01-31 20:31:14 +0100

    usuniecie zakomentowanego kodu i zbednych importow

--- src/main/java/com/example/s21974_bank/S21974BankApplication.java
+++ src/main/java/com/example/s21974_bank/S21974BankApplication.java
@@ -1,21 +1,16 @@
 package com.example.s21974_bank;
 
-import com.example.s21974_bank.service.AccountService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+
 
 @SpringBootApplication
 public class S21974BankApplication {
 
     public static void main(String[] args) {
 
-        //SpringApplication.run(S21974BankApplication.class, args);
-
         ConfigurableApplicationContext context = SpringApplication.run(S21974BankApplication.class);
-
-        AccountService accountService = context.getBean("accountService", AccountService.class);
-
 
     }
 
